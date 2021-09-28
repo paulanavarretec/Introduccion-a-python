1930c397d03fc6d71f114765d170d17c2e177f24
Paula Navarrete <41834362+paulanavarretec@users.noreply.github.com>
2021-09-28 12:16:32 -0300

    solucion
    leer argumentos desde linea comandos

---
+++ velocidad-escape/escape.py
@@ -0,0 +1,12 @@
+import sys
+import math
+
+try:
+  g = float(sys.argv[1])
+  r = float(sys.argv[2])
+
+  print("G:",g," - r:",r)
+
+  print("La velocidad de escape es:", math.sqrt(2*g*r) )
+except:
+  print("Parámetros incorrectos")
