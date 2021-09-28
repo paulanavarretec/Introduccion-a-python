145639d6c592a6de542439a05384e6b874453d29
Paula Navarrete <41834362+paulanavarretec@users.noreply.github.com>
2021-09-28 13:11:56 -0300

    solucion completa

--- rentabilidad/emprendedor1.py
+++ rentabilidad/emprendedor1.py
@@ -1,1 +1,10 @@
 import sys
+
+try:
+  precio_venta = float(sys.argv[1])
+  usuarios = int(sys.argv[2])
+  gastos = int(sys.argv[3])
+
+  print("La utilidad es:", (precio_venta * usuarios)-gastos )
+except:
+  print("Parámetros incorrectos")
