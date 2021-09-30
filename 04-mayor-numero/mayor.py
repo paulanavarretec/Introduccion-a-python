0e47ae34201a539dcb77a1da759de8313ee7a6f9
Paula Navarrete <41834362+paulanavarretec@users.noreply.github.com>
2021-09-30 14:04:16 -0300

    solucion funcionando

--- 04-mayor-numero/mayor.py
+++ 04-mayor-numero/mayor.py
@@ -1,1 +1,8 @@
+import sys
+
+try:
+    mayor = sorted([int(sys.argv[1]), int(sys.argv[2]) , int(sys.argv[3])],reverse=True)[0]
+    print("El número mayor es el {}".format(mayor))
+except:
+    print("Debes ingresar tres numeros")
 
